1e6bc7ce7d83cd13e21838727e61f694fecb113c
akakaplin <105938406+akakaplin@users.noreply.github.com>
2023-05-07 03:05:05 +0000

    Fix

--- download.py
+++ download.py
@@ -7,63 +7,41 @@ from datetime import datetime, timedelta
 import struct
 import csv
 import click
-import pystore
-import dask
-import pandas as pd
+import gzip
 
 
-PYSTORE_FILE = "~/pystore"
-
-def _convert(hour, tick):
-    t = hour + timedelta(microseconds=tick[0])
-
-
-async def download(ticker: str, days: int = 1):
-    pystore.set_path(PYSTORE_FILE)
-    store = pystore.store("FX")
-    collection = store.collection(ticker)
-
+async def download(pair: str, days: int = 1):
     ct = DataCenter(timeout=30, use_cache=True)
-
     now = datetime.now()
-    now = datetime(now.year,now.month,now.day)
+    now = datetime(now.year, now.month, now.day)
     from_date = now - timedelta(days=days)
 
-    hour = from_date
-    while hour <= now:
-        stream = await ct.get_ticks(ticker, hour)
-        out = struct.iter_unpack(ct.format, stream.read())
+    f = open("data.csv.gz", "wb")
+    with gzip.open(f, "wt") as csvfile:
+        datafile_writer = csv.writer(
+            csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        hour = from_date
+        while hour <= now:
+            stream = await ct.get_ticks(pair, hour)
+            out = struct.iter_unpack(ct.format, stream.read())
+            ticks = 0
+            for tick in out:
+                tick = list(tick)
+                tick[0] = hour + timedelta(microseconds=tick[0])
+                datafile_writer.writerow(tick)
+                ticks += 1
+            print(f"Loaded {ticks} ticks at {hour} for {pair}")
+            hour += timedelta(hours=1)
 
-        data = [[hour + timedelta(microseconds=tick[0]), tick[1], tick[2], tick[3], tick[4]] for tick in out]
-        df = pd.DataFrame(columns=['time', 'a', 'b', 'c', 'd'], data=data)
-        
-
-        collection.append(, df)
-        #    print(hour, tick[0], tick[1], tick[2])
-        #    break
-            #collection.write(ticker, aapl[:100], metadata={'source': 'dukascopy'})
-        print(t)
-
-        hour += timedelta(hours=1)
-
-    timerange = (from_date, now)
-
-    print(timerange)
-    print(ticker)
-    #generator = await ct.get_ticks_range(ticker, timerange)
-
-    #for hour, stream in generator:
-    #    print(f'Unpacking data for: {hour}')
-    #    out = struct.iter_unpack(ct.format, stream.read())
-    #    for tick in out:
-    #        print(hour, tick[0], tick[1], tick[2])
 
 @click.command()
 @click.option("--days", default=3, help="Number of days to download.")
 @click.option("--pair", default="EURUSD", help="Pair to download.")
 def run(days: int, pair: str):
     """Download data"""
-    asyncio.run(download(ticker=pair, days=days))
+    asyncio.run(download(pair=pair, days=days))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
--- task.py
+++ task.py
@@ -5,9 +5,11 @@ Test playgorund
 
 from prefect import flow
 
+
 @flow
 def my_favorite_function():
     print("What is your favorite number?")
     return 42
+
 
 print(my_favorite_function())

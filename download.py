872a3ac39c481e1a6b263c82cd80f810a669086f
akakaplin <105938406+akakaplin@users.noreply.github.com>
2023-10-02 18:11:21 +0200

    Fix

--- download.py
+++ download.py
@@ -105,7 +105,7 @@ def run(
             write_api = client.write_api(write_options=SYNCHRONOUS)
 
             def _writer(rows):
-                points = [f'{pair},pair={pair} bid={row[1]},ask={row[2]},bidSize={round(row[3], 4)},askSize={round(row[4], 4)} {int(row[0].timestamp() * 1000)}' for row in rows]
+                points = [f'{influx},pair={pair} bid={row[1]},ask={row[2]},bidSize={round(row[3], 4)},askSize={round(row[4], 4)} {int(row[0].timestamp() * 1000)}' for row in rows]
                 r = write_api.write(bucket=influx, record=points, write_precision=WritePrecision.MS)
                 print(r)
 

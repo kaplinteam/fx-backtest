e705b884987043af13faa73f499d763e3d3c0bfb
akakaplin <105938406+akakaplin@users.noreply.github.com>
2023-10-02 05:11:55 +0200

    Code fix & cleanup

--- influx_check.py
+++ influx_check.py
@@ -6,14 +6,13 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "org"
 url = "http://localhost:8086"
-
-client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-
-
 bucket="fin_1"
 
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
-   
+query_api = client.query_api()
+
+# Write
 for value in range(5):
   point = (
     Point("measurement1")
@@ -23,6 +22,7 @@ for value in range(5):
   write_api.write(bucket=bucket, org="org", record=point)
   time.sleep(1)
 
+# Read (aggregate)
 query = """from(bucket: "fin_1")
   |> range(start: -10m)
   |> filter(fn: (r) => r._measurement == "measurement1")

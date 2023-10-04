ab36b4450465d596df611b094c47d799d27220e8
akakaplin <105938406+akakaplin@users.noreply.github.com>
2023-10-04 03:47:27 +0200

    Speedup

--- task.py
+++ task.py
@@ -18,12 +18,22 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from loader import DataCenter
 
 
-@task(name="dukascopy", 
-    task_run_name="dukascopy-{ticker}-on-{hour:%c}",
-    description="This task loads data from dukascopy & pushes it to the influxdb.")
-def load_hour_data(ticker: str, hour: datetime):
-    """Load data for a hour"""
+def batched(iterable, n):
+    "Batch data into tuples of length n. The last batch may be shorter."
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while (batch := tuple(islice(it, n))):
+        yield batch
 
+
+@task(name="dukascopy", 
+    task_run_name="dukascopy-{ticker}-on-{hours[0]:%c}",
+    description="This task loads data from dukascopy & pushes it to the influxdb.")
+def load_hours_data(ticker: str, hours: list[datetime]):
+    """Load data for a hours"""
+
+    hour = []
     url = os.environ.get("INFLUXDB_HOST", "http://localhost:8086")
     org = os.environ.get("INFLUXDB_ORG", "org")
     bucket = os.environ.get("INFLUXDB_BUCKET", "DUKASCOPY")
@@ -32,24 +42,23 @@ def load_hour_data(ticker: str, hour: datetime):
     with InfluxDBClient(url=url, token=token, org=org, debug=False) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
-        async def download_to_csv(ticker: str, hour: datetime):
+        async def download_to_csv(ticker: str, hours: datetime):
             """Download data & store it to compressed CSV file"""
             ct = DataCenter(timeout=30, use_cache=False)
-            stream = await ct.get_ticks(ticker, hour)
-            out = struct.iter_unpack(ct.format, stream.read())
-            rows = []
-            for row in out:
-                row = list(row)
-                ts = int((hour + timedelta(microseconds=row[0])).timestamp() * 1000)
-                line = f'{ticker} bid={row[1]},ask={row[2]},bidSize={round(row[3], 4)},askSize={round(row[4], 4)} {ts}'
-                rows.append(line)
 
-            write_api.write(bucket=bucket, record=rows, write_precision=WritePrecision.MS)
+            tuples = await data_center.get_ticks_hours(symbol=pair, hours=hours)
+            tuples.sort(key=lambda x: x[0])
+            for tupl in tuples:
+                hour, stream = tupl
+                rows = []
+                for row in struct.iter_unpack(data_center.format, stream.read()):
+                    row = list(row)
+                    ts = int((hour + timedelta(microseconds=row[0])).timestamp() * 1000)
+                    line = f'{ticker} bid={row[1]},ask={row[2]},bidSize={round(row[3], 4)},askSize={round(row[4], 4)} {ts}'
+                    rows.append(line)
+                write_api.write(bucket=bucket, record=rows, write_precision=WritePrecision.MS)
 
-
-        asyncio.run(download_to_csv(ticker=ticker, hour=hour))
-
-    return
+        asyncio.run(download_to_csv(ticker=ticker, hours=hours))
 
 
 @flow(name="Loading tickers for last N days", log_prints=True)
@@ -62,9 +71,8 @@ def load_last_days_depth(tickers: list[str], days: int):
     for day in range(days, 0, -1):
         day = now - timedelta(days=day)
         if day.weekday() < 5:
-            for i in range(0, 24):
-                for ticker in tickers:
-                    load_hour_data(ticker=ticker, hour=day+timedelta(hours=i))
+            for ticker in tickers:
+                load_hours_data(ticker=ticker, hours=[day+timedelta(hours=i) for i in range(0, 24)])
 
 
 @flow(name="Loading tickers for bays between from_days to to_days ago", log_prints=True)
@@ -76,9 +84,8 @@ def load_days_between_depth(tickers: list[str], from_days: int, to_days: int):
     for day in range(from_days, to_days, -1):
         day = now - timedelta(days=day)
         if day.weekday() < 5:
-            for i in range(0, 24):
-                for ticker in tickers:
-                    load_hour_data(ticker=ticker, hour=day+timedelta(hours=i))
+            for ticker in tickers:
+                load_hours_data(ticker=ticker, hours=[day+timedelta(hours=i) for i in range(0, 24)])
 
 
 ALL_TICKERS = [
